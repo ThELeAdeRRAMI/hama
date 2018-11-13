d64011d180d19ce07ce37f1273e2f6a9208b14d7
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-13 18:38:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("507989504888406022")
+let channel =     client.channels.get("511930824090845186")
 setInterval(function() {
-channel.send(`salemsalemsalemsalemsalemsalemsalemsalemsalemsalemsalem`);
+channel.send(`hahahahahahahahahahaahahahahahahahahahahahahahahahahaha`);
 }, 30)
 })
 client.on("message", function(message) {
